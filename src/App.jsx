71ade1fa487a1ecefc1dda1919e8e03aba6e524c
Rodrigo Silva <2231653@my.ipleiria.pt>
2025-03-19 18:30:20 +0000

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
-import { CORE_CONCEPTS } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
 import Header from './components/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import Examples from './components/Examples.jsx';
+
 
 
 
@@ -11,37 +11,16 @@ import TabButton from './components/TabButton.jsx';
 
 function App() {
 
-  function handleSelect(){
-        console.log('Hello World! - Select!')
-    }
+  
 
   return (
-    <div>
-      <Header/>
+    <>
+      <Header />
       <main>
-        <section id="core-concepts">
-        <h2>Core concepts</h2>
-        <ul>
-          <CoreConcept 
-          title= {CORE_CONCEPTS[0].title} 
-          description= {CORE_CONCEPTS[0].description} 
-          image={CORE_CONCEPTS[0].image} />
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
-        </section>
-        <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton onSelect={handleSelect}>Components</TabButton>
-          <TabButton onSelect={handleSelect}>JSX</TabButton>
-          <TabButton onSelect={handleSelect}>Props</TabButton>
-          <TabButton onSelect={handleSelect}>State</TabButton>
-        </menu>
-        </section>
+        <CoreConcepts/>
+        <Examples/>
       </main>
-    </div>
+    </>
   );
 }
 
